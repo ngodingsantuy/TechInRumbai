f99f62385d8848d3ab6afd5738420c7fff016f82
andreyl lahnan <andrey18ti@mahasiswa.pcr.ac.id>
2019-07-24 23:32:01 +0700

    index.php
    tampilan untuk index nya nanti

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="csss/bootstrap.css">
     <title>TechInRumbai</title>
   </head>
   <style>
@@ -18,41 +17,40 @@
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success">
       <div class="container">
       <img src="img/logo2.png" width="5%" class="rounded-circle">
-      <a class="navbar-brand" href="#" >TechInRumbai</a>
+      <a class="navbar-brand" href="index.php" >TechInRumbai</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="#about"></a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item active dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">OperatingSystem</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="Windwows.php">Windwows</a>
-              <a class="dropdown-item" href="Machintos.php">Machintos</a>
-              <a class="dropdown-item" href="Linux.php">Linux</a>
+              <a class="dropdown-item" href="windows.php">Windwows</a>
+              <a class="dropdown-item" href="machintos.php">Machintos</a>
+              <a class="dropdown-item" href="linux.php">Linux</a>
             </div>
           </li>
           <li class="nav-item active dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hardware</a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="member1.php">VGA</a>
-              <a class="dropdown-item" href="member2.php">Processor</a>
-              <a class="dropdown-item" href="member3.php">Komputer Gear</a>
+              <a class="dropdown-item" href="vga.php">VGA</a>
+              <a class="dropdown-item" href="Processor.php">Processor</a>
+              <a class="dropdown-item" href="komputergear.php">Komputer Gear</a>
             </div>
           </li>            
           <li class="nav-item active">
-            <a class="nav-link" href="#SmartPhone">SmartPhone</a>
+            <a class="nav-link" href="smartphone.php">SmartPhone</a>
           </li>                              
           <li class="nav-item active">
-            <a class="nav-link" href="#E-Money">E-Money</a>
+            <a class="nav-link" href="emoney.php">E-Money</a>
           </li>
-<button type="button" class="btn btn-primary" id="tombol" style="">Login</button>
+          <form action="login.php" method="POST">
+<button class="btn btn-primary" id="tombol" type="submit">Login</button>
+          </form>
         </ul>  
       </div>
 </nav>
@@ -64,31 +62,37 @@
 <div class="bd-example">
   <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active" style="color: black;"></li>
       <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
       <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
       <h1>Berita utama</h1>
       <div class="carousel-item active">
-        <img src="img/2.jpg" class="d-block w-100" width="20%">
+        <img src="img/smartphone1.jpg" class="d-block w-100" width="20%">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Samsung Umumkan RAM "Jumbo" untuk Ponsel 5G dan AI</h5>
-          <p>Raksasa teknologi asal Korea Selatan, Samsung, mengumumkan bawha chip  RAM LPDDR5.</p>
+          <div class="badge badge-primary text-wrap">
+            <h5>Xiaomi Luncurkan MIUI 7 Sebentar Lagi/</h5>
+            <p>Kesuksesan Xiaomi sebenarnya bukan hanya dari perangkat hardware mereka, tapi juga dari keberhasilan Custom ROM mereka bernama MIUI yang berhasil menarik banyak peminat dan membentuk komunitas pengguna yang luas dari berbagai belahan dunia./</p>
+          </div>          
         </div>
       </div>
       <div class="carousel-item">
-        <img src="img/5.jpg" class="d-block w-100" width="20%">
+        <img src="img/os1.jpg" class="d-block w-100" width="20%">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Akhir Bulan, Huawei Y9 Prime 2019 Resmi Meluncur</h5>
-          <p>Huawei akan meluncurkan smartphone terbarunya yakni Y9 Prime 2019.</p>
+            <div class="badge badge-primary text-wrap">
+          <h5>Update Windows 10, Pengguna Tidak Perlu Login Pakai Password</h5>
+          <p>Jakarta - Microsoft tampaknya akan segera menghilangkan fitur kata sandi (password) perlahan-lahan.</p>
+          </div>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="img/9.jpg" class="d-block w-100" width="20%">
+        <img src="img/99.jpg" class="d-block w-100" width="20%">
         <div class="carousel-caption d-none d-md-block">
+            <div class="badge badge-primary text-wrap">
           <h5>Cepat Habis? Tenang, Ini 4 Alasan Pilih E-Money Daripada Uang Tunai</h5>
           <p>Kamu hanya memerlukan aplikasi uang digital atau cashless, lalu tap dengan kartu atau membayar melalui aplikasi ke semua outlet yang bekerjasama.</p>
+          </div>
         </div>
       </div>
     </div>
@@ -97,7 +101,7 @@
       <span class="sr-only">Previous</span>
     </a>
     <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="carousel-control-next-icon" aria-hidden="true" style="color: black"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
@@ -128,63 +132,25 @@
      <div class="row">
        <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Aplikasi Populer Dihapus dari Toko Mac App Store</h5>
+    <p class="card-text">Aplikasi Populer Dihapus dari Toko Mac App Store</p>
+    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
   </div>
 </div>
      </div>
      <div class="row">
      <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Pendiri Linux: Facebook, Twitter, dan Instagram adalah Sampah</h5>
+    <p class="card-text">Jakarta - Linus Torvalds mentereng namanya di jagat teknologi sebagai pencipta sistem operasi Open Source, Linux. Ia dikenal suka blak-blakan dan belum lama ini mengutarakan kritik keras pada media sosial.
+</p>
+    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
   </div>
 </div>
     </div>
   </div>
 </div>
 </div>
-</section>
-<div class="container"style="
-    margin-top: 50px;">
-    <h2>Berita terlama</h2>
-  </div>
-<section id="Berita" class="Berita">
-    <div class="container mt-5">
-      <div class="row mb-2">
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">World</strong>
-          <h3 class="mb-0">Featured post</h3>
-          <div class="mb-1 text-muted">Nov 12</div>
-          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong>
-          <h3 class="mb-0">Post title</h3>
-          <div class="mb-1 text-muted">Nov 11</div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
 </section>
 <section id="Portfolio" class="Portfolio pb-4">
   <div>
@@ -229,10 +195,10 @@
             </div>
           </div>                             
         </div>
-<div class="row mb-4">
+<h2>SmartPhone</h2>
+<div class="row mb-4-dark">
           <div class="col-md">
           <div class="card" style="width: 18rem;">
-            <h2>SmartPhone</h2>
             <img src="img/5.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Akhir Bulan, Huawei Y9 Prime 2019 Resmi Meluncur</h5>
@@ -277,14 +243,16 @@
     <h5 class="card-title">Mandiri Sebut Gangguan Juga Pengaruhi Top-up Saldo e-Money</h5>
     <p class="card-text"> Corporate secretary Bank Mandiri Rohan Hafas menjelaskan gangguan yang sempat dialami nasabah juga berimbas pada pengguna layanan uang elektronik, e-money. Kendati tetap bisa digunakan untuk bertransaksi, pengguna tidak bisa mengisi ulang saldo e-money.</p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
   </div>
 </div>
 <div class="card">
-    <img src="img/9.jpg" class="card-img-top" alt="...">
+    <img src="img/99.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Cepat Habis? Tenang, Ini 4 Alasan Pilih E-Money Daripada Uang Tunai</h5>
     <p class="card-text">Di zaman serba digital ini amat mudah melakukan transaksi keuangan ketimbang memakai uang tunai. Kamu hanya memerlukan aplikasi uang digital atau cashless, lalu tap dengan kartu atau membayar melalui aplikasi ke semua outlet yang bekerjasama.</p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
   </div>
 
 </div>
@@ -304,6 +272,7 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>    
-  </body>
+    <script src="jss/bootstrap.min.js"></script>    
+  <!-- Code injected by live-server -->
+</body>
 </html>
