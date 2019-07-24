d8a13f111af542758bffeb593254b14858983f08
abdul arif pratama <abdul18ti@mahasiswa.pcr.ac.id>
2019-07-25 00:23:12 +0700

    Create smartphone.php
    php untuk menambahkan artikel

---
+++ smartphone.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="csss/bootstrap.css">
+
+    <title>TechInRumbai</title>
+  </head>
+  <body>
+<section id="Home" class="Home">
+  <div class="container">   
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success">
+      <div class="container">
+      <img src="img/logo2.png" width="5%" class="rounded-circle">
+      <a class="navbar-brand" href="index.php" >TechInRumbai</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item active dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">OperatingSystem</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="windows.php">Windwows</a>
+              <a class="dropdown-item" href="machintos.php">Machintos</a>
+              <a class="dropdown-item" href="linux.php">Linux</a>
+            </div>
+          </li>
+          <li class="nav-item active dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hardware</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="vga.php">VGA</a>
+              <a class="dropdown-item" href="Processor.php">Processor</a>
+              <a class="dropdown-item" href="komputergear.php">Komputer Gear</a>
+            </div>
+          </li>            
+          <li class="nav-item active">
+            <a class="nav-link" href="smartphone.php">SmartPhone</a>
+          </li>                              
+          <li class="nav-item active">
+            <a class="nav-link" href="emoney.php">E-Money</a>
+          </li>
+          <form action="login.php" method="POST">
+<button class="btn btn-primary" id="tombol" type="submit">Login</button>
+          </form>
+        </ul>  
+      </div>
+</nav>
+</div>
+</section>
+<section >
+  <div class="container"style="
+    margin-top: 100px;">
+    <div class="jumbotron jumbotron-fluid">
+    <h1 class="display-4">SmartPhone</h1>
+    <p class="lead">Berita seputar SmartPhone.</p>
+</div>
+  </div>
+</section>
+ <div class="container">
+        <div class="row mb-4 pt-4">
+ <div class="container"style="
+    margin-top: 50px;">
+  </div>         
+        </div>
+        <div class="row mb-4">
+          <div class="col-md">
+            <div class="card" style="width: 18rem;">
+            <img src="img/2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Samsung Umumkan RAM "Jumbo" untuk Ponsel 5G dan AI</h5>
+              <p class="card-text">Raksasa teknologi asal Korea Selatan, Samsung, mengumumkan bawha chip  RAM LPDDR5 dengan kapasitas 12 GB bikinannya telah siap untuk diproduksi secara massal menjelang akhir bulan ini.</p>
+              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            </div>
+          </div>
+          </div>
+          <div class="col-md">
+          <div class="card" style="width: 18rem;">
+            <img src="img/3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Laptop Gaming HP Omen X 2S Mendukung Layar Sekunder Seluas 6 Inci
+
+</h5>
+              <p class="card-text">Laptop gaming memang miliki penggemarnya sendiri, dan jika Anda sedang mencari laptop gaming baru, tampaknya laptop gaming terbaru milik HP ini bisa menjadi pilihan.</p>
+              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            </div>
+          </div>
+          </div> 
+          <div class="col-md">
+            <div class="card" style="width: 18rem;">
+              <img src="img/4.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">AMD Bawa Dukungan FreeSync dan 6 Multi-Monitor Ala Eyefinity Di Linux Kernel 5.0</h5>
+                <p class="card-text">Radeon FreeSync merupakan salah satu fitur andalan dari AMD yang memungkinkan anda untuk memainkan berbagai macam game termasuk nonton video dengan lebih lancar dengan minim stuttering maupun tearing, kabar baiknya AMD telah menghadirkan dukungan fitur Radeon FreeSync ke operasi sistem GNU/Linux yang menggunakan Linux kernel 5.0. </p>
+                <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+              </div>
+            </div>
+          </div>                             
+        </div>
+<div class="row mb-4-dark">
+          <div class="col-md">
+          <div class="card" style="width: 18rem;">
+            <img src="img/5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Akhir Bulan, Huawei Y9 Prime 2019 Resmi Meluncur</h5>
+              <p class="card-text">Huawei akan meluncurkan smartphone terbarunya yakni Y9 Prime 2019 pada akhir bulan Juli ini. Dilansir dari GSMArena, India menjadi negara pertama yang beruntung mendapatkan jatah Huawei Y9 Prime 2019.</p>
+              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            </div>
+          </div>
+          </div>
+          <div class="col-md">
+           <div class="card" style="width: 18rem;">
+            <img src="img/6.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Tampilan Nokia 7.2 Terungkap, Begini Wujudnya</h5>
+              <p class="card-text">Nokia sedang mempersiapkan smartphone baru dengan kode nama Nokia TA-119, yang digadang-gadang akan keluar sebagai Nokia 7.2. Smartphone baru ini terungkap melalui sebuah desain Soft Case yang di ungkap oleh slash leaks.</p>
+              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+            </div>
+          </div>
+          </div> 
+          <div class="col-md">
+            <div class="card" style="width: 18rem;">
+              <img src="img/7.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                 <h5 class="card-title">Xiaomi Mi A3 Masuk Pasar Asia 31 Juli, Malaysia Duluan</h5>
+                 <p class="card-text">Beberapa hari lalu, Xiaomi Mi A3 diluncurkan secara global. Saat ini, Mi A3 sudah bisa di pre-order di Spanyol. Selanjutnya penjualan perdana Mi A3 akan dimulai pada 24 Juli.</p>
+                 <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+              </div>
+            </div>
+          </div>                             
+        </div>        
+      </div>
+</section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="jss/bootstrap.min.js"></script>
+  </body>
+</html>
