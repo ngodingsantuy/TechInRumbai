6c0eeb30bc601e46d58b70a428a2562f8cda6a22
andreyl lahnan <andrey18ti@mahasiswa.pcr.ac.id>
2019-07-24 23:35:38 +0700

    koneksi.php
    menghubungkan ke database

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$name_database = "techinrumbai";
+
+$db = mysqli_connect("$server", "$user", "$password", "$name_database");
+
+if (!$db ) {
+	die("gagal terhubung dengan database: " . mysqli_connect_error());	
+}
+
+?>
