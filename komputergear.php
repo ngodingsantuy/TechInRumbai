63c35bf6f9e96e64761da51bcf405b89fba1419d
andreylahnan <andrey18ti@mahasiswa.pcr.ac.id>
2019-07-25 09:08:43 +0700

    komputergear.php
    beberapa perubahan pada kodingan komputergear.

--- komputergear.php
+++ komputergear.php
@@ -11,6 +11,7 @@
     <title>TechInRumbai</title>
   </head>
   <body>
+    <?php include "koneksi.php"; ?>
 <section id="Home" class="Home">
   <div class="container">   
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success">
@@ -60,84 +61,34 @@
     margin-top: 100px;">
     <div class="jumbotron jumbotron-fluid">
     <h1 class="display-4">Komputer Gear</h1>
-    <p class="lead">Berita seputar Hardware Komputer Gear.</p>
+    <p class="lead">Berita seputar Komputer Gear.</p>
 </div>
   </div>
 </section>
- <div class="container">
-        <div class="row mb-4 pt-4">
- <div class="container"style="
-    margin-top: 50px;">
-  </div>         
-        </div>
-        <div class="row mb-4">
-          <div class="col-md">
-            <div class="card" style="width: 18rem;">
-            <img src="img/2.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Samsung Umumkan RAM "Jumbo" untuk Ponsel 5G dan AI</h5>
-              <p class="card-text">Raksasa teknologi asal Korea Selatan, Samsung, mengumumkan bawha chip  RAM LPDDR5 dengan kapasitas 12 GB bikinannya telah siap untuk diproduksi secara massal menjelang akhir bulan ini.</p>
-              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-            </div>
-          </div>
-          </div>
-          <div class="col-md">
-          <div class="card" style="width: 18rem;">
-            <img src="img/3.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Laptop Gaming HP Omen X 2S Mendukung Layar Sekunder Seluas 6 Inci
+<?php        
+    $i=0;
+    $query=mysqli_query($db,"SELECT * FROM artikel where kategori='processor'");
+    while ($m = mysqli_fetch_array($query)) 
+    { ?>
 
-</h5>
-              <p class="card-text">Laptop gaming memang miliki penggemarnya sendiri, dan jika Anda sedang mencari laptop gaming baru, tampaknya laptop gaming terbaru milik HP ini bisa menjadi pilihan.</p>
-              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-            </div>
-          </div>
-          </div> 
-          <div class="col-md">
-            <div class="card" style="width: 18rem;">
-              <img src="img/4.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">AMD Bawa Dukungan FreeSync dan 6 Multi-Monitor Ala Eyefinity Di Linux Kernel 5.0</h5>
-                <p class="card-text">Radeon FreeSync merupakan salah satu fitur andalan dari AMD yang memungkinkan anda untuk memainkan berbagai macam game termasuk nonton video dengan lebih lancar dengan minim stuttering maupun tearing, kabar baiknya AMD telah menghadirkan dukungan fitur Radeon FreeSync ke operasi sistem GNU/Linux yang menggunakan Linux kernel 5.0. </p>
-                <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-              </div>
-            </div>
-          </div>                             
-        </div>
-<div class="row mb-4-dark">
-          <div class="col-md">
-          <div class="card" style="width: 18rem;">
-            <img src="img/5.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Akhir Bulan, Huawei Y9 Prime 2019 Resmi Meluncur</h5>
-              <p class="card-text">Huawei akan meluncurkan smartphone terbarunya yakni Y9 Prime 2019 pada akhir bulan Juli ini. Dilansir dari GSMArena, India menjadi negara pertama yang beruntung mendapatkan jatah Huawei Y9 Prime 2019.</p>
-              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-            </div>
-          </div>
-          </div>
-          <div class="col-md">
-           <div class="card" style="width: 18rem;">
-            <img src="img/6.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Tampilan Nokia 7.2 Terungkap, Begini Wujudnya</h5>
-              <p class="card-text">Nokia sedang mempersiapkan smartphone baru dengan kode nama Nokia TA-119, yang digadang-gadang akan keluar sebagai Nokia 7.2. Smartphone baru ini terungkap melalui sebuah desain Soft Case yang di ungkap oleh slash leaks.</p>
-              <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-            </div>
-          </div>
-          </div> 
-          <div class="col-md">
-            <div class="card" style="width: 18rem;">
-              <img src="img/7.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                 <h5 class="card-title">Xiaomi Mi A3 Masuk Pasar Asia 31 Juli, Malaysia Duluan</h5>
-                 <p class="card-text">Beberapa hari lalu, Xiaomi Mi A3 diluncurkan secara global. Saat ini, Mi A3 sudah bisa di pre-order di Spanyol. Selanjutnya penjualan perdana Mi A3 akan dimulai pada 24 Juli.</p>
-                 <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-              </div>
-            </div>
-          </div>                             
-        </div>        
-      </div>
-</section>
+    <?php
+                    $i++;
+                        ?>
+<?php
+$isi=substr($m['isi'],1,200);
+  ?>
+<div class="container">
+<div class="card">
+    <img src="img/<?php echo $m['gambar'] ?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $m['judul']; ?></h5>
+    <p class="card-text"><?php echo $isi; ?>......</p>
+    <p class="card-text"><small class="text-muted"><?php echo $m['tanggal'] ?></small></p>
+    <a href="tampilartikel.php?id=<?php print $m['id_artikel']?>" class="btn btn-primary">Lihat Selengkapnya</a>
+  </div>
+</div>
+</div>
+                    <?php } ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="jss/bootstrap.min.js"></script>
